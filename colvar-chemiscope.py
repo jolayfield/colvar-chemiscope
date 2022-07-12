712aba182de73ea32dfec92f673d2239985b88dd
jolayfield@gmail.com <jolayfield@gmail.com>
2022-07-12 15:40:16 -0500

    first commit

---
+++ colvar-chemiscope.py
@@ -0,0 +1,155 @@
+import numpy as np 
+import ase
+import ase.io
+from chemiscope import write_input
+import plumed
+import sys
+import argparse
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+# Create the parser
+my_parser = argparse.ArgumentParser(description='Generate a chemiscope json.gz file from a trajectory')
+
+# Add the arguments
+my_parser.add_argument('Trajectory',
+                       metavar='traj',
+                       type=str,
+                       help='the pdb trajectory file (make sure to check atom name so they all start with a letter.)')
+
+my_parser.add_argument('CVfile',
+                      metavar='cv_file',
+                      type=str,
+                      help='The file listing the CVs to calculate. They should be written in `name: [CV definition]` style.')
+my_parser.add_argument('Output',
+                      metavar='output_file',
+                      type=str,
+                      help='The full name of the chemiscope file (it should end in `.json.gz`)')
+my_parser.add_argument('-p','--plumed', required=False
+                      )
+
+# Execute the parse_args() method
+args = my_parser.parse_args()
+
+traj_file = args.Trajectory
+
+cv_file = args.CVfile
+
+out_file = args.Output
+
+with open(cv_file) as f:
+    cvs = f.readlines()
+CVs = [i for i in cvs if i !='\n']
+  
+
+nCVs = len(CVs)
+
+for CV in CVs:
+   if 'PATH' in CV:
+       nCVs += 1
+
+
+print(f'Making a Chemiscope file with {nCVs} CVs. They are :\n{CVs}')
+
+
+cv_np = np.zeros((nCVs,1))
+
+
+# Read in trajectory using ase
+traj = ase.io.read(traj_file,':')
+
+# Setup plumed object to do calculation
+p = plumed.Plumed()
+p.cmd("setMDEngine","python")
+p.cmd("setTimestep", 1.)
+p.cmd("setKbT", 1.)
+natoms = len(traj[0].positions)
+p.cmd("setNatoms",natoms)
+p.cmd("setLogFile","test.log")
+p.cmd("init")
+
+# Read plumed input 
+if args.plumed:
+    p.cmd("readInputLine",f'INCLUDE FILE={args.plumed}')
+else:
+    p.cmd("readInputLine",f"MOLINFO STRUCTURE={traj_file}") 
+
+names=[]
+n=0
+
+print('CVS',CVs)
+
+for CV in CVs:
+    name = CV.split()[0].strip(':')
+    p.cmd("readInputLine", CV)
+    
+    if 'PATH' in CV:
+        names.append(name+'.s')
+        shape = np.zeros(1, dtype=np.int_)
+        p.cmd(f"getDataRank {name}.s",shape)
+        p.cmd(f"setMemoryForData {name}.s",cv_np[n])
+        n += 1
+        names.append(name+'.z')
+        p.cmd(f"getDataRank {name}.z", shape)
+        p.cmd(f"setMemoryForData {name}.z", cv_np[n])
+        n+=1
+      
+    else:
+        names.append(name)
+        # Get the shape of the value calculated in plumed
+        shape = np.zeros( 1, dtype=np.int_ )
+        p.cmd(f"getDataRank {name}", shape )
+
+        # Now setup some memory to hold the variable that is shared 
+        # between plumed and the underlying code
+        p.cmd(f'setMemoryForData {name}', cv_np[n])
+        n += 1
+
+
+
+print('names',names)
+# # Loop over trajectory and get data from plumed
+nfram, tt, vs, box = 0, [], [[] for x in range(nCVs)], np.array([[100.,0,0],[0,100.,0],[0,0,100]])
+charges, forces, virial = np.zeros(natoms,dtype=np.float64), np.zeros([natoms,3]), np.zeros((3,3),dtype=np.float64)
+
+for ts in traj :
+    p.cmd("setStep",nfram)
+    p.cmd("setBox",box )
+    p.cmd("setMasses", ts.get_masses() )
+    p.cmd("setCharges", charges )
+    pos = np.array(ts.get_positions(), dtype=np.float64 )
+    p.cmd("setPositions", pos )
+    p.cmd("setForces", forces )
+    p.cmd("setVirial", virial )
+    p.cmd("calc")
+    tt.append(nfram)
+
+    for cv in range(nCVs):
+        vs[cv].append(cv_np[cv,0])
+        
+    nfram = nfram + 1
+    
+atom_dict= {'H':1, 'C':6, 'N':7, 'O':8, 'S':16, '1':1, '2':1,'3':1}
+
+
+for frame in traj:
+    frame.numbers = np.array(
+        [
+            atom_dict[am[0]] 
+            for am in frame.arrays["atomtypes"]
+        ]
+    )
+
+    
+# This constructs the dictionary of properties for chemiscope
+properties = {}  
+properties["time"]={"target": "structure","values": tt,"description": "Simulation step number"}
+for i,name in enumerate(names):
+
+    properties[name]= {'target':'structure', "values":vs[i], "description":'fix this later'} #CVs[i]}
+
+
+# # This generates our chemiscope output
+write_input(out_file, frames=traj, properties=properties )
